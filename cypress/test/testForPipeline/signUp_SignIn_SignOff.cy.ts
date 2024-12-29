c8fb46afd451f516e2e8ec62160ac1690ca21bed
sachin-ade24 <120191253+sachin-ade24@users.noreply.github.com>
2024-12-29 23:04:13 +0530

    New updates

--- cypress/test/testForPipeline/signUp_SignIn_SignOff.cy.ts
+++ cypress/test/testForPipeline/signUp_SignIn_SignOff.cy.ts
@@ -1,9 +1,12 @@
+/*Important points*/
 /*1) This is a pipeline test where we want to execute all tests back-to-back
   2) For this we need a minimal test for sign-out after the account creation i.e. sign-up
   3) Though, we can use it within the sign-up script but, it will be good 
      if we create a separate test for this
   4) After that we can do the sign-in
   5) Finally, after block in the e2e.ts will be executed
+  6) For the pipeline, we can use GitHub Actions.
+     We can create a YAML file and run the workflow.
 */
 /*Note: It is not necessary to create such a file for pipeline.
         We can run indivisual scripts for pipeline.
