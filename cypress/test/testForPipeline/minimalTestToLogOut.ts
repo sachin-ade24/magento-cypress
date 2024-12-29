b32507cca978f244f96b98a35437fb782131a3e8
sachin-ade24 <120191253+sachin-ade24@users.noreply.github.com>
2024-12-29 22:34:41 +0530

    Updated minimal test

--- cypress/test/testForPipeline/minimalTestToLogOut.ts
+++ cypress/test/testForPipeline/minimalTestToLogOut.ts
@@ -1,9 +1,9 @@
-import { signOut } from "cypress/pom/signInPage/methods/methods";
+import { signOff } from "cypress/pom/signInPage/methods/methods";
 
 //This test is created for pipeline execution
 describe("Log out: minimal test", () => {
   it("Executes minimal logout test", () => {
     cy.log("It's a minimal logout test");
-    signOut();
+    signOff();
   });
 });
